fd49caa0f0240bfdee114128b30594f3bc727ae0
Ayman <aymalsh@gmail.com>
2019-08-16 11:39:41 +0300

    initial commit plus a couple of exercises

---
+++ MyFirstXamarin/MyFirstXamarin/QuotesPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyFirstXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        public int Index { get; set; }
+        private string[] Quotes { get; set; }
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            Quotes = new string[]
+            {
+                "First quote",
+                "Second quote",
+                "Third quote",
+                "Forthquote"
+            };
+            Index = 0;
+            quotes.Text = Quotes[Index];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Index++;
+            if (Index > Quotes.Length-1)
+            {
+                Index = 0;
+            }
+            quotes.Text = Quotes[Index];
+        }
+    }
+}
---
+++ MyFirstXamarin/MyFirstXamarin/XamarinLogin.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyFirstXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XamarinLogin : ContentPage
+    {
+        public XamarinLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
